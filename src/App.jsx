f6017f28683573ffac8d69d710351df199c567f8
Karan samrat <karansamrat05039@gmail.com>
2024-02-19 14:42:47 +0530

    redux added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
- 
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-    <h1 className='bg-red-700'>hello</h1>
-    </>
-  )
+  useEffect(() => {
+    authService
+      .getCuurentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-sc"> this is blog website</div>
+  ) : null;
 }
 
-export default App
+export default App;
